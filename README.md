4273472bd80577ddab0c07ccd692e35f1beac7a2
Jérémy Lal <kapouer@melix.org>
2012-03-15 17:34:23 +0100

    Consolidate use of script src and notemplate attributes. Release 0.0.2

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 express-notemplate
 ==================
 
-What ?
-------
+Let the template be plain HTML code, and use javascript to merge data into it.
 
-* templates are pristine html
-* no artifical language or syntax, plain DOM scripts
-* functions that merge data on server can also be used to merge data on browsers,
-  whenever possible or useful.
-* jQuery-like library to help manipulation and portability.
+Why ?
+-----
+
+The view is totally separated from the model.
+The glue code that merges data into the view is easy to maintain, and can be run by the server or the clients.
+There is no artificial syntax, no unnecessary abstraction.
+Just DOM and javascript.
+
+In particular, this allows one to merge new data on the clients, using the exact same code.
+
+The only extra is a jQuery $ object provided by default on server.
+(if the javascript code is not used on clients, jQuery is not needed on clients).
 
 
 Setup
@@ -27,30 +33,42 @@ Setup
 Usage
 -----
 
-As usual in express, define locals :
+It is meant to be used as any other express view rendering :
 
 	res.local('mydata', mydata);
+	res.render('index');
 
-Add handlers inside your html :
+Then express-notemplate will load the html file into a DOM, add window.$ to it, and process script tags :
 
+	<script type="text/javascript">
+		// some client code here, won't be run on server
+	</script>
+	<script type="text/javascript" notemplate="both" src="javascripts/moment.js"></script>
 	<script type="text/javascript" notemplate="server">
 		function _merge(data) {
-			$('head > title').text(data.mydata.title);
+			$('head > title').text(data.mydata.title + moment());
 		}
 	</script>
-	<script type="text/javascript">
+	<script type="text/javascript" notemplate="both">
 		function merge(data) {
 			$('body').html(data.mydata.body);
 		}
 	</script>
 
-The script tag with notemplate="server" attribute will call _merge(), then be removed from output.
-Then merge() is called.
 
-Client-side browsers will have access to window.merge, not to window._merge.
+In this example :
 
-A modified version of [jQuip](https://github.com/mythz/jquip)
-is available to the server-side scripts, allowing easy DOM manipulation and traversal.
+* moment.js is loaded and the script tag is kept in the html output,
+* _merge is run and the script tag containing it is removed from html output,
+* merge is run and kept in the output.
+
+script tags can have attribute notemplate = server | client | both :
+
+* (default) client : script are not run
+* server : scripts are run and tag is removed
+* both : scripts are run
+
+The "notemplate" attribute is removed from html output.
 
 
 Dependencies
@@ -60,9 +78,9 @@ Ideally express-notemplate should use upstream jsdom and jquip (or another jquer
 Unfortunately :
 
 * jsdom 0.2.12 does not output XHTML, putting most browsers in quirks mode.
-  and also has a small querySelectorAll bug.
+  and also has an unpleasant querySelectorAll bug
 
-* jquip cannot be used as-is with jsdom
+* A (patched for jsdom) version of [jQuip](https://github.com/mythz/jquip) is available to server-side scripts.
 
 So forks are used until a better situation is found.
 
--- notemplate.js
+++ notemplate.js
@@ -1,25 +1,57 @@
-(function(exports) {
 var jsdom = require('jsdom');
+var Path = require('path');
+var URL = require('url');
+var readfile = require('fs').readFileSync;
 
 exports.compile = function(str, opts) {
 	// use opts.features, opts.scripts ?
 	var window = jsdom.jsdom(str, null, {
 		features: {
-			FetchExternalResources: false,				// mandatory !
-			MutationEvents: '1.0',								// tell jsdom to run
-			ProcessExternalResources: ['script'],	// <script> contents
-			QuerySelector: true										// rely on bundled Sizzle, until jQuery becomes modular
+			FetchExternalResources: false,				// loaded depending on script[notemplate] attribute
+			ProcessExternalResources: false,			// same
+			MutationEvents: false,								// not needed
+			QuerySelector: true										// jquip doesn't provide querySelector/querySelectorAll compatibility, Sizzle does.
 		}
 	}).createWindow();
 
 	// core jQuery : selector, manipulation, traversal
 	// use real jQuery when it becomes modular.
 	// jquip needs some patches to run inside jsdom (mainly because node.style.key is not supported by cssom)
-	var jquip = require('fs').readFileSync(require.resolve('jquip-jsdom')).toString();
-	window.run(jquip);
+	run(window, require.resolve('jquip-jsdom'));
 
-	// remove <script notemplate="server">
-	window.$('script[notemplate=server]').remove();
+	// <script> tags can have attribute notemplate = server | client | both
+	// default value is client
+	// server value : scripts are fetched (if they have src attribute), and discarded
+	// client value : script are not fetched
+	// both : scripts are fetched (if they have src attribute )and not discarded
+	var public = Path.join('.', opts.public || 'public');
+	var scripts = window.$('script');
+	for (var i=0, len = scripts.length; i < len; i++) {
+		var script = scripts[i];
+		var att = script.attributes.notemplate;
+		if (!att) continue; // default is notemplate="client"
+		att = att.value;
+		script.attributes.removeNamedItem('notemplate'); // make sure attribute is removed
+		if (att != "server" && att != "both") continue; // any other value is "client"
+		var src = script.attributes.src;
+		if (!src && script.textContent) window.run(script.textContent); // html5 runs script content only when src is not set
+		if (att == "server") window.$(script).remove(); // remove script tag
+		if (!src) continue;
+		// load file and run it
+		src = src.value;
+		// try to find javascript file
+		var url = URL.parse(src);
+		if (url.hostname) {
+			console.error("express-notemplate doesn't allow loading external URL -- ping author to do it", script);
+			continue;
+		}
+		var path = Path.join(public, url.pathname);
+		if (!Path.existsSync(path)) {
+			console.error("express-notemplate doesn't find script.src file", path);
+			continue;
+		}
+		run(window, path);
+	}
 	
 	return function(data) {
 		// just return the template if no merge functions
@@ -30,4 +62,7 @@ exports.compile = function(str, opts) {
 		return window.document.doctype.toString() + "\n" + window.document.outerHTML;
 	};
 };
-})((typeof exports === "undefined") ? window : exports);
+
+function run(window, path) {
+	window.run(readfile(path).toString());
+}
