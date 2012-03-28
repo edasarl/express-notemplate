54a8f6303b3199e4abd4ca08620098a2a49e603d
Jérémy Lal <kapouer@melix.org>
2012-03-28 17:07:50 +0200

    Prepare for version 0.0.5

--- README.md
+++ README.md
@@ -9,9 +9,10 @@ Why ?
 The view is totally separated from the model.
 The glue code that merges data into the view is easy to maintain, and can be run by the server or the clients.
 There is no artificial syntax, no unnecessary abstraction.
-Just DOM and javascript.
+Just familiar DOM and javascript.
 
-In particular, this allows one to merge new data on the clients, using the exact same code.
+In particular, this allows one to merge new data on the clients using a messenging system using the exact same code
+as what is needed on the server to output html.
 
 The only extra is a jQuery $ object provided by default on server.
 (if the javascript code is not used on clients, jQuery is not needed on clients).
@@ -45,22 +46,22 @@ Then express-notemplate will load the html file into a DOM, add window.$ to it, 
 	</script>
 	<script type="text/javascript" notemplate="both" src="javascripts/moment.js"></script>
 	<script type="text/javascript" notemplate="server">
-		function _merge(data) {
+		$(document).on('data', function(e, data) {
 			$('head > title').text(data.mydata.title + moment());
-		}
+		});
 	</script>
 	<script type="text/javascript" notemplate="both">
-		function merge(data) {
+		$(document).on('data', function(e, data) {
 			$('body').html(data.mydata.body);
-		}
+		});
 	</script>
 
 
 In this example :
 
 * moment.js is loaded and the script tag is kept in the html output,
-* _merge is run and the script tag containing it is removed from html output,
-* merge is run and kept in the output.
+* the first handler is run on server but won't be available on client
+* the second handler is run and it will be possible to trigger it on client too.
 
 script tags can have attribute notemplate = server | client | both :
 
@@ -71,16 +72,23 @@ script tags can have attribute notemplate = server | client | both :
 The "notemplate" attribute is removed from html output.
 
 
-Dependencies
-------------
+Middleware
+----------
 
-Ideally express-notemplate should use upstream jsdom and jquip (or another jquery-like lib).
-Unfortunately :
+	var notemplate = require('express-notemplate');
+	notemplate.on('data', function(window, data, opts) {
+		// opts are the options of compile(str, opts)
+		// this is called *before* any other template-bound handlers
+	});
+	notemplate.on('render', function(window, data, opts) {
+		// this is called *after* any other template-bound handlers
+	});
 
-* jsdom 0.2.12 does not output XHTML, putting most browsers in quirks mode.
-  and also has an unpleasant querySelectorAll bug
+	Typical example of this are the notemplate-rootpath and notemplate-minify middlewares.
 
-* A (patched for jsdom) version of [jQuip](https://github.com/mythz/jquip) is available to server-side scripts.
 
-So forks are used until a better situation is found.
+Features
+--------
 
+* console.log works in the jsdom context.
+* globally defined event handlers for all templates :
--- notemplate.js
+++ notemplate.js
@@ -3,15 +3,20 @@ var Path = require('path');
 var URL = require('url');
 var readfile = require('fs').readFileSync;
 
+// arrays of handlers
+var handlers = [];
+
 exports.compile = function(str, opts) {
 	// use opts.features, opts.scripts ?
 	opts = opts || {};
+	opts.public = opts.public || 'public';
+	
 	var window = jsdom.jsdom(str, null, {
 		features: {
 			FetchExternalResources: false,				// loaded depending on script[notemplate] attribute
 			ProcessExternalResources: false,			// same
 			MutationEvents: false,								// not needed
-			QuerySelector: true										// jquip doesn't provide querySelector/querySelectorAll compatibility, Sizzle does.
+			QuerySelector: false									// not needed, we use jquery's bundled sizzle instead of jsdom's one.
 		}
 	}).createWindow();
 
@@ -19,14 +24,13 @@ exports.compile = function(str, opts) {
 	// core jQuery : selector, manipulation, traversal
 	// use real jQuery when it becomes modular.
 	// jquip needs some patches to run inside jsdom (mainly because node.style.key is not supported by cssom)
-	run(window, require.resolve('jquip-jsdom'));
+	run(window, Path.join(__dirname, 'node_modules/jquery-browser/lib/jquery.js'));
 
 	// <script> tags can have attribute notemplate = server | client | both
 	// default value is client
 	// server value : scripts are fetched (if they have src attribute), and discarded
 	// client value : script are not fetched
 	// both : scripts are fetched (if they have src attribute )and not discarded
-	var public = Path.join('.', opts.public || 'public');
 	var scripts = window.$('script');
 	for (var i=0, len = scripts.length; i < len; i++) {
 		var script = scripts[i];
@@ -40,26 +44,17 @@ exports.compile = function(str, opts) {
 		if (att == "server") window.$(script).remove(); // remove script tag
 		if (!src) continue;
 		// load file and run it
-		src = src.value;
-		// try to find javascript file
-		var url = URL.parse(src);
-		if (url.hostname) {
-			console.error("express-notemplate doesn't allow loading external URL -- ping author to do it", script);
-			continue;
-		}
-		var path = Path.join(public, url.pathname);
-		if (!Path.existsSync(path)) {
-			console.error("express-notemplate doesn't find script.src file", path);
-			continue;
-		}
-		run(window, path);
+		var path = resolve(opts.public, src.value);
+		if (path) run(window, path);
 	}
 	
 	return function(data) {
-		// just return the template if no merge functions
-		if (!window._merge && !window.merge) return str;
-		if (window._merge) window._merge(data);
-		if (window.merge) window.merge(data);
+		// global handlers
+		triggerHandler('data', window, data, opts);
+		// no handlers return undefined
+		var lastHandlerValue = window.$(window.document).triggerHandler('data', data);
+		// global handlers
+		triggerHandler('render', window, data, opts);
 		// output selected nodes
 		if (opts.fragment) return outer(window.$(opts.fragment));
 		// outputs doctype because of jsdom bug
@@ -67,9 +62,40 @@ exports.compile = function(str, opts) {
 	};
 };
 
+exports.on = function(type, handler) {
+	// allows adding "middleware" before compile() is called by render()
+	// event can be 'data' in which case the handler is called before other handlers, or 'render',
+	// in which case the handler is called after all other handlers, that is, before html output
+	handlers.forEach(function(o) {
+		if (o.type == type && o.handler == handler) return; // register handlers only once
+	});
+	handlers.push({type:type, handler:handler});
+};
+
+function triggerHandler(type, window, data, opts) {
+	handlers.forEach(function(o) {
+		if (o.type == type) o.handler(window, data, opts);
+	});
+}
+
 function run(window, path) {
 	window.run(readfile(path).toString());
 }
+
+function resolve(public, src) {
+	var url = URL.parse(src);
+	if (url.hostname) {
+		console.error("express-notemplate doesn't allow loading external URL -- ping author to do it", script);
+		return null;
+	}
+	var path = Path.join('.', public, url.pathname);
+	if (!Path.existsSync(path)) {
+		console.error("express-notemplate doesn't find script.src file", path);
+		return null;
+	}
+	return path;
+}
+
 function outer($nodes) {
 	var ret = '';
 	$nodes.each(function() {
